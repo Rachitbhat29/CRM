a6732b38ff5eb5074bebdde234302b20bac9fba5
Rachitbhat29 <36357104+Rachitbhat29@users.noreply.github.com>
2019-05-01 15:10:16 +0530

    Add files via upload

--- CRM/models.py
+++ CRM/models.py
@@ -1,10 +1,26 @@
-from CRM import db
+from CRM import db,login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(20), unique=True, nullable = False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def  __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}','{self.password}')"
 
 class Customer(db.Model):
     cust_id = db.Column(db.Integer, primary_key= True)
     first_name = db.Column(db.String(20), unique=True, nullable =False)
     last_name = db.Column(db.String(20), unique=True, nullable=False)
-    image_file = db.Column(db.String(20),nullable=False, default='default.jpg')
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60),nullable=False)
     posts = db.relationship('Addresses', backref = 'customerdata', lazy= True)
 
